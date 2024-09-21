ed1544c8f5a1bba1b9f9bb8f762e88d3f97d9b61
piyushsingh114 <piyush38756@gmail.com>
2024-09-21 13:53:10 +0530

    righttriangle

---
+++ Lecture04/pattern02.java
@@ -0,0 +1,16 @@
+package Lecture04;
+
+public class pattern02 {
+    public static void main(String[] args){
+        int n=8;
+     for (int i=1;i<=n;i++)
+        {
+        
+        for( int j=1;j<=i;j++)
+        System.out.print("*");
+        System.out.println();
+
+    }
+    
+}
+}
